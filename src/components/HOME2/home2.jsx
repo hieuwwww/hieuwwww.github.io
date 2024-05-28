f20b85fc335b2229d2d93d4c7b08cad2d65b6560
primaldev <binhbestleeseen@gmail.com>
2024-05-28 13:15:27 +0700

    sent to thg lon trau - thang lou troll

--- src/components/HOME2/home2.jsx
+++ src/components/HOME2/home2.jsx
@@ -2,7 +2,7 @@ import "./home2.css";
 export default function Home2() {
   return (
     <div className="home2-video">
-      <video controls>
+      <video autoPlay loop muted>
         <source src="/videos/home2.mp4" type="video/mp4" />
       </video>
     </div>
--- src/components/HOME3/home3.jsx
+++ src/components/HOME3/home3.jsx
@@ -1,46 +1,48 @@
 import "./home3.css";
 export default function Home3({ src }) {
+  const toleft = { left: '0' }
   return (
     <div className="container_home3">
-      <h1>add text here</h1>
+      <h1 style={{ color: '#ffffff', marginBottom: '50px' }}>Học tập đa dạng - hiệu quả</h1>
       <div className="container_home3_it itemUp">
-        <div className="item_home3 it1">
+        <div className="item_home3 it1" >
           <img src={src} alt="" />
-          <a href="">
-            toán
-            <i class="fa-solid fa-angles-right"></i>
+          <a href="" >
+            Toán lớp 1
+            <i className="fa-solid fa-angles-right"></i>
           </a>
         </div>
         <div className="item_home3 it2">
           <img src={src} alt="" />
-          <a href="">
-            <i></i>
+          <a href="" >
+            Toán lớp 1
+            <i className="fa-solid fa-angles-right"></i>
           </a>
         </div>
         <div className="item_home3 it3">
           <img src={src} alt="" />
-          <a href="">
-            <i></i>
+          <a href="" > Toán lớp 1
+            <i className="fa-solid fa-angles-right"></i>
           </a>
         </div>
       </div>
       <div className="container_home3_it itemDown">
         <div className="item_home3 it4">
           <img src={src} alt="" />
-          <a href="">
-            <i></i>
+          <a href="" > Toán lớp 1
+            <i className="fa-solid fa-angles-right"></i>
           </a>
         </div>
         <div className="item_home3 it51">
           <img src={src} alt="" />
-          <a href="">
-            <i></i>
+          <a href="" > Toán lớp 1
+            <i className="fa-solid fa-angles-right"></i>
           </a>
         </div>
         <div className="item_home3 it6">
           <img src={src} alt="" />
-          <a href="">
-            <i></i>
+          <a href="" > Toán lớp 1
+            <i className="fa-solid fa-angles-right"></i>
           </a>
         </div>
       </div>
