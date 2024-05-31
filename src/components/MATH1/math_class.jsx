69e68247f170efee24b045b5affe7b4d377ff3e9
hieuwwww <wibutrua@gmail.com>
2024-05-31 16:03:22 +0700

    thêm router, nội dung các lớp

---
+++ src/components/MATH1/math_class.jsx
@@ -0,0 +1,34 @@
+import NextPage from "../FOOTER/nextpage";
+import NavBarOfficial from "../OfficialHome/NavBar";
+import csss from "./math1.module.css";
+import VIDEO from "./videomath";
+
+export default function MathClass({ title, background_image, data }) {
+  console.log(data);
+  return (
+    <div style={{ marginLeft: "25%", marginTop: "8%" }}>
+      <h2
+        style={{
+          backgroundImage: `url("${background_image}")`,
+          height: "300px",
+          width: "86%",
+          backgroundSize: "cover",
+        }}
+      ></h2>
+      <h2 style={{ marginTop: "80px", color: "#488dc7" }}>{title}</h2>
+      <div
+        style={{ display: "flex", flexWrap: "wrap", paddingBottom: "200px" }}
+      >
+        {data &&
+          data.map((item, index) => (
+            <VIDEO
+              key={index}
+              title={item.title}
+              video_link={item.video_link}
+              ly_thuyet={item.ly_thuyet}
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/math_class_data.js
@@ -0,0 +1,316 @@
+const class_content_1 = [
+  {
+    title: "Bài 1: khái niệm - luyện tập vận dụng 3 - toán 1 | mathfun",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 2",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 3",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 4",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 5",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 6",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 7",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 8",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 9",
+    video_link: "",
+    ly_thuyet: "",
+  },
+];
+const class_content_2 = [
+  {
+    title: "Bài 1: khái niệm - luyện tập vận dụng 3 - toán 1 | mathfun",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 2",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 3",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 4",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 5",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 6",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 7",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 8",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 9",
+    video_link: "",
+    ly_thuyet: "",
+  },
+];
+const class_content_3 = [
+  {
+    title: "Bài 1: khái niệm - luyện tập vận dụng 3 - toán 1 | mathfun",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 2",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 3",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 4",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 5",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 6",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 7",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 8",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 9",
+    video_link: "",
+    ly_thuyet: "",
+  },
+];
+const class_content_4 = [
+  {
+    title: "Bài 1: khái niệm - luyện tập vận dụng 3 - toán 1 | mathfun",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 2",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 3",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 4",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 5",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 6",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 7",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 8",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 9",
+    video_link: "",
+    ly_thuyet: "",
+  },
+];
+const class_content_5 = [
+  {
+    title: "Bài 1: khái niệm - luyện tập vận dụng 3 - toán 1 | mathfun",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 2",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 3",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 4",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 5",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 6",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 7",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 8",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 9",
+    video_link: "",
+    ly_thuyet: "",
+  },
+];
+const class_content_dethi = [
+  {
+    title: "Bài 1: khái niệm - luyện tập vận dụng 3 - toán 1 | mathfun",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 2",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 3",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 4",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 5",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 6",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 7",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 8",
+    video_link: "",
+    ly_thuyet: "",
+  },
+  {
+    title: "Bài 9",
+    video_link: "",
+    ly_thuyet: "",
+  },
+];
+
+export const math_class_data = [
+  {
+    title: "Toán lớp 1",
+    link_to: "/toan1",
+    background_image: "/pics/nentoanlop1.jpg",
+    class_content: class_content_1,
+  },
+  {
+    title: "Toán lớp 2",
+    link_to: "/toan2",
+    class_content: class_content_2,
+  },
+  {
+    title: "Toán lớp 3",
+    link_to: "/toan3",
+    class_content: class_content_3,
+  },
+  {
+    title: "Toán lớp 4",
+    link_to: "/toan4",
+    class_content: class_content_4,
+  },
+  {
+    title: "Toán lớp 5",
+    link_to: "/toan5",
+    class_content: class_content_5,
+  },
+  {
+    title: "Đề thi toán tổng hợp",
+    link_to: "/dethi",
+    class_content: class_content_dethi,
+  },
+];
