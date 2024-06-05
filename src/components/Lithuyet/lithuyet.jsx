3e273ac58758886cd5beeda5666d8a91af1cd1f0
hieuwwww <wibutrua@gmail.com>
2024-06-05 12:45:07 +0700

    fixed mapping error

--- src/components/Lithuyet/lithuyet.jsx
+++ src/components/Lithuyet/lithuyet.jsx
@@ -1,73 +1,79 @@
-import './lithuyet.css'
-import React, { useEffect } from 'react';
-// import { dataLiThuyet1 } from './lithuyet_data';
+import "./lithuyet.css";
+import React, { useEffect } from "react";
 
 export default function Lithuyet({ src, data }) {
-    useEffect(() => {
-        const handleScroll = () => {
-            var div = document.getElementById('container_video_lithuyet');
-            if (div) {
-                var scrollY = window.scrollY || window.pageYOffset;
+  useEffect(() => {
+    const handleScroll = () => {
+      var div = document.getElementById("container_video_lithuyet");
+      if (div) {
+        var scrollY = window.scrollY || window.pageYOffset;
 
-                if (scrollY > 30) { // Điều kiện khi cuộn hơn 200px
-                    div.classList.add('shrink');
-                } else {
-                    div.classList.remove('shrink');
-                }
-            }
-        };
-        // console.log(data);
+        if (scrollY > 30) {
+          // Điều kiện khi cuộn hơn 30px
+          div.classList.add("shrink");
+        } else {
+          div.classList.remove("shrink");
+        }
+      }
+    };
 
-        window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        // Dọn dẹp sự kiện cuộn khi component unmount
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    // Dọn dẹp sự kiện cuộn khi component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    console.log(data);
-    return (
-
-        <div className='container_lithuyet' >
-
-            <div id='container_video_lithuyet' >
-                <iframe className="videoYT" width="100%" height="100%" src={src} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-            </div>
-            <div id='noidungLithuyet' className="container_noidung_lithuyet">
-                <ul>
-                    {data && data.map(e => {
-                        return (
-                            <>
-                                <h2 key={e.title.toString()}>{e.title}</h2>
-                                {e.subtitle.map((temp, index) => {
-                                    return (
-                                        < >
-                                            <h3 key={temp.toString()}>{temp}</h3>
-                                            {e.noidung.map((x) => {
-                                                return (
-
-                                                    <p key={x.toString()} >{x}</p>
-
-                                                );
-                                            })}
-
-                                            <img src={e.srcpic[index]} alt={temp} />
-                                            <p style={{ color: '#488dc7', textAlign: 'center' }}>Hình ảnh minh họa</p>
-                                            <div className='containerBlooket'>
-                                                <a className='blooket_link' href={e.blooket[index]} target='_blank' ><img style={{ width: '100px', height: '100px' }} src={e.blooketPics[index]} alt={e.blooketPics[index]} /></a>
-                                                <div className='clicktoplay'>Ấn vào để chơi</div>
-                                            </div>
-                                        </>
-
-                                    );
-                                })}
-                            </>
-                        );
-
-                    })}
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="container_lithuyet">
+      <div id="container_video_lithuyet">
+        <iframe
+          className="videoYT"
+          width="100%"
+          height="100%"
+          src={src}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div id="noidungLithuyet" className="container_noidung_lithuyet">
+        <ul>
+          <li>
+            <h2>{data.title}</h2>
+            {data.subtitle.map((subtitle, subIndex) => (
+              <div key={subIndex}>
+                <h3>{subtitle}</h3>
+                {subIndex < data.noidung.length && (
+                  <p>{data.noidung[subIndex]}</p>
+                )}
+                <img src={data.srcpic[subIndex]} alt={subtitle} />
+                <p style={{ color: "#488dc7", textAlign: "center" }}>
+                  Hình ảnh minh họa
+                </p>
+                <div className="containerBlooket">
+                  <a
+                    className="blooket_link"
+                    href={data.blooket[subIndex]}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      style={{ width: "100px", height: "100px" }}
+                      src={data.blooketPics[subIndex]}
+                      alt={data.blooketPics[subIndex]}
+                    />
+                  </a>
+                  <div className="clicktoplay">Ấn vào để chơi</div>
+                </div>
+              </div>
+            ))}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
