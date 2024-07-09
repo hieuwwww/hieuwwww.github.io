94aed19666551b8661680bf5dae3969b431008ee
hieuwwww <wibutrua@gmail.com>
2024-07-09 17:14:41 +0700

    lop3 + 4 content

--- src/math_class_data.js
+++ src/math_class_data.js
@@ -3,76 +3,64 @@ const class_content_1 = [
     title: "Chương 1 - Bài 1: Làm quen với một số hình",
     video_link: "https://www.youtube.com/embed/0vVLAXQUF5s?si=m1r-A_16cr0KM2Hn",
     ly_thuyet: "/toan1/clip1",
-    // 
+    //
   },
   {
     title: "Chương 1 - Bài 2: Các số từ 0 đến 10 và  Nhiều hơn, ít hơn",
     video_link: "https://www.youtube.com/embed/Cnb32kAqV0A?si=tyZnJ1IsCdppn9sf",
     ly_thuyet: "/toan1/clip2",
     tolinkvideo: "",
-
   },
   {
     title: "Chương 1 - Bài 3: So sánh số , mấy và mấy ",
     video_link: "https://www.youtube.com/embed/Dnj5FglKgKk?si=cKiVd14_e_vgxLnl",
     ly_thuyet: "/toan1/clip3",
-
   },
   {
     title: "Chương 1 - Bài 4: Phép cộng , trừ trong phạm vi 10 ",
     video_link: "https://www.youtube.com/embed/3fi7uwBU-CE?si=FfKjJlAqux43hIxL",
     ly_thuyet: "/toan1/clip4",
-
   },
   {
     title: "Chương 1 - Bài 5: Bảng cộng, trừ trong phạm vi 10 ",
     video_link: "https://www.youtube.com/embed/c7Yv9izJH5Y?si=4y4Ln8s6HcDEbQk2",
     ly_thuyet: "/toan1/clip5",
-
   },
   {
     title: "Chương 2 - Bài 1: Số có hai chữ số ",
     video_link: "https://www.youtube.com/embed/Gqr4iw9eJCI?si=mN2KE66AzdGkUxkv",
     ly_thuyet: "/toan1/clip6",
-
   },
   {
     title: "Chương 2 - Bài 2: So sánh số có hai chữ số",
     video_link: "https://www.youtube.com/embed/XpPpBPUiplg?si=XpyI-I7vUev3v5lE",
     ly_thuyet: "/toan1/clip7",
-
   },
   {
     title: "Chương 2 - Bài 3: Bảng các số từ 1 đến 100 ",
     video_link: "https://www.youtube.com/embed/_WpDZ3OmS6I?si=fcafAt3Q7x6MWel0",
     ly_thuyet: "/toan1/clip8",
-
   },
   {
     title: "Chương 2 - Bài 4: Dài hơn, ngắn hơn, đơn vị đo độ dài",
     video_link: "https://www.youtube.com/embed/x5CNJUMJcII?si=Qy7mBDe28QUHZJ4C",
     ly_thuyet: "/toan1/clip9",
-
   },
   {
     title: "Chương 2 - Bài 5: Phép tính với số có hai chữ số",
     video_link: "https://www.youtube.com/embed/x5CNJUMJcII?si=Qy7mBDe28QUHZJ4C",
     ly_thuyet: "/toan1/clip10",
-
   },
   {
     title: "Chương 2 - Bài 6: Xem giờ đúng trên đồng hồ",
     video_link: "https://www.youtube.com/embed/x5CNJUMJcII?si=Qy7mBDe28QUHZJ4C",
     ly_thuyet: "/toan1/clip11",
-
   },
   {
     title: "Chương 2 - Bài 7: Các ngày trong tuần",
     video_link: "https://www.youtube.com/embed/x5CNJUMJcII?si=Qy7mBDe28QUHZJ4C",
     ly_thuyet: "/toan1/clip12",
-
   },
-
 ];
 
 const class_content_2 = [
@@ -112,7 +100,8 @@ const class_content_2 = [
     ly_thuyet: "/toan2/clip7",
   },
   {
-    title: "Học kì 1 - Bài 8: Phép cộng,trừ (có nhớ) số có hai chữ số với số có một chữ số",
+    title:
+      "Học kì 1 - Bài 8: Phép cộng,trừ (có nhớ) số có hai chữ số với số có một chữ số",
     video_link: "",
     ly_thuyet: "/toan2/clip8",
   },
@@ -142,7 +131,8 @@ const class_content_2 = [
     ly_thuyet: "/toan2/clip13",
   },
   {
-    title: "Học kì 2 - Bài 14: Đơn vị, chục, trăm, nghìn, Các số tròn trăm, tròn chục",
+    title:
+      "Học kì 2 - Bài 14: Đơn vị, chục, trăm, nghìn, Các số tròn trăm, tròn chục",
     video_link: "",
     ly_thuyet: "/toan2/clip14",
   },
@@ -162,7 +152,8 @@ const class_content_2 = [
     ly_thuyet: "/toan2/clip17",
   },
   {
-    title: "Học kì 2 - Bài 18: Đề-xi-mét, mét, ki-lô-mét, Giới thiệu tiền Việt Nam",
+    title:
+      "Học kì 2 - Bài 18: Đề-xi-mét, mét, ki-lô-mét, Giới thiệu tiền Việt Nam",
     video_link: "",
     ly_thuyet: "/toan2/clip18",
   },
@@ -189,96 +180,505 @@ const class_content_2 = [
 ];
 const class_content_3 = [
   {
-    title: "Bài 1: khái niệm - luyện tập vận dụng 3 - toán 1 | mathfun",
+    title: "Bài 1: Tìm thành phần trong phép cộng, phép trừ | mathfun",
     video_link: "",
     ly_thuyet: "/toan3/clip1",
   },
   {
-    title: "Bài 2",
+    title: "Bài 2: Ôn tập bảng nhân 2; 5, bảng chia 2; 5 | mathfun",
     video_link: "",
     ly_thuyet: "/toan3/clip2",
   },
   {
-    title: "Bài 3",
+    title: "Bài 3: Bảng nhân 3, bảng chia 3 | mathfun",
     video_link: "",
     ly_thuyet: "/toan3/clip3",
   },
   {
-    title: "Bài 4",
+    title: "Bài 4: Bảng nhân 4, bảng chia 4 | mathfun",
     video_link: "",
     ly_thuyet: "/toan3/clip4",
   },
   {
-    title: "Bài 5",
+    title: "Bài 5: Bảng nhân 6, bảng chia 6 | mathfun",
     video_link: "",
     ly_thuyet: "/toan3/clip5",
   },
+
   {
-    title: "Bài 6",
+    title: "Bài 6: Bảng nhân 7, bảng chia 7 | mathfun",
     video_link: "",
     ly_thuyet: "/toan3/clip6",
   },
   {
-    title: "Bài 7",
-    video_link: "",
-    ly_thuyet: "/toan3/clip7",
-  },
-  {
-    title: "Bài 8",
+    title: "Bài 8: Bảng nhân 8, bảng chia 8 | mathfun",
     video_link: "",
     ly_thuyet: "/toan3/clip8",
   },
   {
-    title: "Bài 9",
+    title: "Bài 9: Bảng nhân 9, bảng chia 9 | mathfun",
     video_link: "",
     ly_thuyet: "/toan3/clip9",
+  },
+  {
+    title: "Bài 10: Tìm thành phần trong phép nhân, phép chia | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip10",
+  },
+  {
+    title: "Bài 11: Một phần mấy | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip11",
+  },
+  {
+    title: "Bài 11: Điểm ở giữa, trung điểm của đoạn thẳng | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip5",
+  },
+  {
+    title:
+      "Bài 12: Hình tròn. Tâm, bán kính, đường kính của hình tròn | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip12",
+  },
+  {
+    title: "Bài 13: Góc, góc vuông, góc không vuông | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip13",
+  },
+  {
+    title:
+      "Bài 14: Hình tam giác, hình tứ giác. Hình chữ nhật, hình vuông | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip14",
+  },
+  {
+    title: "Bài 15: Khối lập phương, khối hộp hình chữ nhật | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip15",
+  },
+  {
+    title: "Bài 16: Nhân số có hai chữ số với số có một chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip16",
+  },
+  {
+    title: "Bài 17: Gấp một số lên một số lần | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip17",
+  },
+  {
+    title: "Bài 18: Phép chia hết, phép chia có dư | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip18",
+  },
+  {
+    title: "Bài 19: Chia số có hai chữ số cho số có một chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip19",
+  },
+  {
+    title: "Bài 20:  Giảm một số đi một số lần | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip20",
+  },
+  {
+    title: "Bài 21: Bài toán giải bằng hai bước tính | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip21",
+  },
+  {
+    title: "Bài 22: Mi - li - mét | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip22",
+  },
+  {
+    title: "Bài 23: Gam | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip23",
+  },
+  {
+    title: "Bài 24: Mi - li -lít | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip24",
+  },
+  {
+    title: "Bài 25: Nhiệt độ, Đơn vị đo nhiệt độ | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip25",
+  },
+  {
+    title: "Bài 26: Nhân số có ba chữ số với số có một chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip26",
+  },
+  {
+    title: "Bài 27: Chia số có ba chữ số cho số có một chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip27",
+  },
+  {
+    title: "Bài 28: Biểu thức số. Tính giá trị của biểu thức số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip28",
+  },
+  {
+    title: "Bài 29: So sánh số lớn gấp mấy lần số bé | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip29",
+  },
+  {
+    title: "Bài 30: Các số có bốn chữ số. Số 10000 | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip30",
+  },
+  {
+    title: "Bài 31: So sánh các số trong phạm vi 10000 | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip31",
+  },
+  {
+    title: "Bài 32: Làm quen với chữ số La Mã | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip32",
+  },
+  {
+    title: "Bài 33: Làm tròn số đến hàng chục, hàng trăm | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip33",
+  },
+  {
+    title:
+      "Bài 34: Chu vi hình tam giác, hình tứ giác, hình chữ nhật, hình vuông | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip34",
+  },
+  {
+    title: "Bài 35: Diện tích của một hình. Xăng - ti - mét vuông | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip35",
+  },
+  {
+    title: "Bài 36: Diện tích hình chữ nhật, diện tích hình vuông | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip36",
+  },
+  {
+    title: "Bài 37: Phép cộng trong phạm vi 10000 | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip37",
+  },
+  {
+    title: "Bài 38: Phép trừ trong phạm vi 10000 | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip38",
+  },
+  {
+    title: "Bài 39: Nhân số có bốn chữ số với một số có một chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip39",
+  },
+  {
+    title: "Bài 40: Chia số có bốn chữ số cho số có một chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip40",
+  },
+  {
+    title: "Bài 41: Các số có năm chữ số. Số 100000 | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip41",
+  },
+  {
+    title: "Bài 42: So sánh các số trong phạm vi 100000 | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip42",
+  },
+  {
+    title: "Bài 43: Làm tròn số đến hàng nghìn, hàng chục nghìn | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip43",
+  },
+  {
+    title: "Bài 44: Phép cộng trong phạm vi 100000 | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip44",
+  },
+  {
+    title: "Bài 45: Phép trừ trong phạm vi 100000 | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip45",
+  },
+  {
+    title: "Bài 46: Xem đồng hồ. Tháng - năm | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip46",
+  },
+  {
+    title: "Bài 47: Thực hành xem đồng hồ, xem lịch | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip47",
+  },
+  {
+    title: "Bài 48: Tiền Việt Nam | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip48",
+  },
+  {
+    title: "Bài 49: Nhân số có năm chữ số với số có một chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip49",
+  },
+  {
+    title: "Bài 50: Chia số có năm chữ số cho số có một chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip50",
+  },
+  {
+    title:
+      "Bài 51: Thu thập, phân loại, ghi chép số liệu. Bảng số liệu | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip51",
+  },
+  {
+    title: "Bài 52: Khả năng xảy ra của một sự kiện | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan3/clip52",
   },
 ];
 const class_content_4 = [
   {
-    title: "Bài 1: khái niệm - luyện tập vận dụng 3 - toán 1 | mathfun",
+    title: "Bài 1: Số chẵn, số lẻ | mathfun",
     video_link: "",
     ly_thuyet: "/toan4/clip1",
   },
   {
-    title: "Bài 2",
+    title: "Bài 2: Biểu thức chứa chữ | mathfun",
     video_link: "",
     ly_thuyet: "/toan4/clip2",
   },
   {
-    title: "Bài 3",
+    title: "Bài 3: Giải bài toán có ba bước tính | mathfun",
     video_link: "",
     ly_thuyet: "/toan4/clip3",
   },
+
   {
-    title: "Bài 4",
+    title: "Bài 4: Đo góc, đơn vị đo góc | mathfun",
     video_link: "",
     ly_thuyet: "/toan4/clip4",
   },
   {
-    title: "Bài 5",
+    title: "Bài 5: Góc nhọn, góc tù, góc bẹt | mathfun",
     video_link: "",
     ly_thuyet: "/toan4/clip5",
   },
   {
-    title: "Bài 6",
+    title: "Bài 6: Số có sáu chữ số. Số 1 000 000 | mathfun",
     video_link: "",
     ly_thuyet: "/toan4/clip6",
   },
   {
-    title: "Bài 7",
+    title: "Bài 7: Hàng và lớp | mathfun",
     video_link: "",
     ly_thuyet: "/toan4/clip7",
   },
   {
-    title: "Bài 8",
+    title: "Bài 8: Các số trong phạm vi lớp triệu | mathfun",
     video_link: "",
     ly_thuyet: "/toan4/clip8",
   },
   {
-    title: "Bài 9",
+    title: "Bài 9: Làm tròn số đến hàng trăm nghìn | mathfun",
     video_link: "",
     ly_thuyet: "/toan4/clip9",
+  },
+  {
+    title: "Bài 10: So sánh các số có nhiều chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip10",
+  },
+  {
+    title: "Bài 11: Làm quen với dãy số tự nhiên | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip11",
+  },
+
+  {
+    title: "Bài 12: Yến, tạ, tấn | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip12",
+  },
+  {
+    title: "Bài 13: Đề-xi-mét vuông, mét vuông, mi-li-mét vuông | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip13",
+  },
+  {
+    title: "Bài 14: Giây, thế kỉ | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip14",
+  },
+
+  {
+    title: "Bài 15: Phép cộng các số có nhiều chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip15",
+  },
+  {
+    title: "Bài 16: Phép trừ các số có nhiều chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip16",
+  },
+  {
+    title: "Bài 17: Tính chất giao hoán và kết hợp của phép cộng | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip17",
+  },
+  {
+    title: "Bài 18: Tìm hai số biết tổng và hiệu của hai số đó | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip18",
+  },
+
+  {
+    title: "Bài 19: Hai đường thẳng vuông góc | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip19",
+  },
+
+  {
+    title: "Bài 20: Hai đường thẳng song song | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip20",
+  },
+
+  {
+    title: "Bài 21: Hình bình hành, hình thoi | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip21",
+  },
+
+  {
+    title: "Bài 22: Nhân với số có một chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip22",
+  },
+  {
+    title: "Bài 23: Chia cho số có một chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip23",
+  },
+  {
+    title: "Bài 24: Tính chất giao hoán và kết hợp của phép nhân | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip24",
+  },
+  {
+    title: "Bài 25: Nhân, chia với 10, 100, 1000, ... | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip25",
+  },
+  {
+    title:
+      "Bài 26: Tính chất phân phối của phép nhân đối với phép cộng | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip26",
+  },
+  {
+    title: "Bài 27: Nhân với số có hai chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip27",
+  },
+  {
+    title: "Bài 28: Chia cho số có hai chữ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip28",
+  },
+
+  {
+    title: "Bài 29: Tìm số trung bình cộng | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip29",
+  },
+  {
+    title: "Bài 30: Bài toán liên quan đến rút về đơn vị | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip30",
+  },
+  {
+    title: "Bài 31: Bài toán liên quan đến tỉ số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip31",
+  },
+  {
+    title: "Bài 32: Dãy số liệu thống kê| mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip32",
+  },
+  {
+    title: "Bài 33:  Biểu đồ cột | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip33",
+  },
+  {
+    title: "Bài 34: Số lần xuất hiện của một sự kiện| mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip34",
+  },
+  {
+    title: "Bài 35: Khái niệm phân số| mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip35",
+  },
+  {
+    title: "Bài 36: Phân số và phép chia số tự nhiên| mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip36",
+  },
+  {
+    title: "Bài 37: Tính chất cơ bản của phân số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip37",
+  },
+  {
+    title: "Bài 38: Rút gọn phân số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip38",
+  },
+  {
+    title: "Bài 39: Quy đồng mẫu số các phân số| mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip39",
+  },
+  {
+    title: "Bài 40: So sánh phân số| mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip40",
+  },
+  {
+    title: "Bài 41: Phép cộng phân số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip41",
+  },
+  {
+    title: "Bài 42: Phép trừ phân số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip42",
+  },
+  {
+    title: "Bài 43: Phép nhân phân số| mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip43",
+  },
+  {
+    title: "Bài 44: Phép chia phân số | mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip44",
+  },
+  {
+    title: "Bài 45: Tìm phân số của một số| mathfun",
+    video_link: "",
+    ly_thuyet: "/toan4/clip45",
   },
 ];
 const class_content_5 = [
